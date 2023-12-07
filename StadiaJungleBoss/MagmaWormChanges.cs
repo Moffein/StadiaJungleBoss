ffb9e6e5f7723af1f49662295689d1d04b7bcd71
Moffein <22742925+Moffein@users.noreply.github.com>
2023-12-06 23:53:24 -0800

    Changed worm name.

---
+++ StadiaJungleBoss/MagmaWormChanges.cs
@@ -0,0 +1,111 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using R2API;
+using RoR2;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StadiaJungleBoss
+{
+    internal static class MagmaWormChanges
+    {
+        public static bool enabled, useStatItem;
+
+        public static ItemDef StatItem;
+
+        private static BodyIndex MagmaWormIndex;
+
+        public static void Initialize()
+        {
+            if (!enabled) return;
+            InitItem();
+            RoR2Application.onLoad += GetBodyIndex;
+
+            On.RoR2.CharacterBody.GetDisplayName += ReplaceName;
+            On.RoR2.CharacterBody.GetSubtitle += ReplaceSubtitle;
+            IL.RoR2.Util.GetBestBodyName += SuppressMendingName;
+        }
+
+        private static void SuppressMendingName(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(
+                     x => x.MatchCallvirt(typeof(CharacterBody), "HasBuff")
+                    );
+            c.Emit(OpCodes.Ldloc_0);
+            c.EmitDelegate<Func<BuffIndex, CharacterBody, BuffIndex>>((buffIndex, body) =>
+            {
+                if (buffIndex == DLC1Content.Buffs.EliteEarth.buffIndex && CheckForWorm(body))
+                {
+                    return BuffIndex.None;
+                }
+                return buffIndex;
+            });
+        }
+
+        private static string ReplaceSubtitle(On.RoR2.CharacterBody.orig_GetSubtitle orig, CharacterBody self)
+        {
+            if (CheckForWorm(self))
+            {
+                return "STADIAJUNGLEBOSS_BODY_SUBTITLE";
+            }
+            return orig(self);
+        }
+
+        private static string ReplaceName(On.RoR2.CharacterBody.orig_GetDisplayName orig, CharacterBody self)
+        {
+            if (CheckForWorm(self))
+            {
+                return "STADIAJUNGLEBOSS_BODY_NAME";
+            }
+            return orig(self);
+        }
+
+        private static void GetBodyIndex()
+        {
+            MagmaWormIndex = BodyCatalog.FindBodyIndex("MagmaWormBody");
+        }
+
+        private static void InitItem()
+        {
+            if (!useStatItem) return;
+            StatItem = ScriptableObject.CreateInstance<ItemDef>();
+            StatItem.name = "StadiaBossMagmaWormItem";
+            StatItem.deprecatedTier = ItemTier.NoTier;
+            StatItem.nameToken = "Stadia Worm";
+            StatItem.pickupToken = "Changes the attacks of Magma Worms.";
+            StatItem.descriptionToken = "Changes the attacks of Magma Worms.";
+            StatItem.tags = new[]
+            {
+                ItemTag.WorldUnique,
+                ItemTag.BrotherBlacklist,
+                ItemTag.CannotSteal
+            };
+            ItemDisplayRule[] idr = new ItemDisplayRule[0];
+            //ContentAddition.AddItemDef(OriginBonusItem);
+            ItemAPI.Add(new CustomItem(StatItem, idr));
+        }
+
+        private static bool CheckForWorm(CharacterBody body)
+        {
+            if (body.bodyIndex == MagmaWormIndex && body.inventory)
+            {
+                if (useStatItem)
+                {
+                    return body.inventory.GetItemCount(StatItem) > 0;
+                }
+                else
+                {
+                    if (body.inventory.GetItemCount(RoR2Content.Items.TeleportWhenOob) > 0
+                        && body.inventory.GetEquipmentIndex() == StadiaJungleBossPlugin.EliteEarthEquipment.equipmentIndex
+                        && SceneManager.GetActiveScene().name == "rootjungle")
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- StadiaJungleBoss/StadiaJungleBossPlugin.cs
+++ StadiaJungleBoss/StadiaJungleBossPlugin.cs
@@ -7,12 +7,13 @@ using StadiaJungleBoss.Setup;
 using UnityEngine.AddressableAssets;
 using StadiaJungleBoss.Components;
 using System;
+using BepInEx.Configuration;
 
 namespace StadiaJungleBoss
 {
     [BepInDependency("com.bepis.r2api")]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
-    [BepInPlugin("com.Moffein.StadiaJungleBoss", "Stadia Jungle Boss", "1.0.0")]
+    [BepInPlugin("com.Moffein.StadiaJungleBoss", "Stadia Jungle Boss", "1.1.0")]
     [R2APISubmoduleDependency(new string[]
     {
         "PrefabAPI",
@@ -21,13 +22,30 @@ namespace StadiaJungleBoss
     public class StadiaJungleBossPlugin : BaseUnityPlugin
     {
         public static PluginInfo pluginInfo;
+
+        internal static EquipmentDef EliteEarthEquipment = Addressables.LoadAssetAsync<EquipmentDef>("RoR2/DLC1/EliteEarth/EliteEarthEquipment.asset").WaitForCompletion();
+
         private void Awake()
         {
             pluginInfo = this.Info;
             Assets.PopulateAssets();
             Tokens.LoadLanguage();
+
+            ReadConfig();
+
             On.RoR2.SceneDirector.Start += StageSetup.SceneDirectorHook;
             RoR2Application.onLoad += CreateSpawnCard;
+
+            MagmaWormChanges.Initialize();
+        }
+
+        private void ReadConfig()
+        {
+            MagmaWormChanges.enabled = base.Config.Bind<bool>(new ConfigDefinition("Magma Worm", "Enable Changes"), true,
+                new ConfigDescription("Modify the Mending Magma Worm spawned by this mod.")).Value;
+
+            MagmaWormChanges.useStatItem = base.Config.Bind<bool>(new ConfigDefinition("Magma Worm", "Use Hidden Item"), true,
+                new ConfigDescription("Use a hidden item to determine whether to apply Magma Worm changes. If not, changes will be applied using a hackier method. You should REALLY leave this enabled unless you are using a mod with a config based on raw item indexes.")).Value;
         }
 
         private void CreateSpawnCard()
@@ -44,17 +62,38 @@ namespace StadiaJungleBoss
             spawnCard.nodeGraphType = RoR2.Navigation.MapNodeGroup.GraphType.Ground;
             spawnCard.hullSize = HullClassification.Golem;
 
-            spawnCard.itemsToGrant = new ItemCountPair[]
+            if (MagmaWormChanges.enabled && MagmaWormChanges.useStatItem)
             {
-                new ItemCountPair()
+
+                spawnCard.itemsToGrant = new ItemCountPair[]
                 {
-                    itemDef = RoR2Content.Items.TeleportWhenOob,
-                    count = 1
-                }
-            };
+                    new ItemCountPair()
+                    {
+                        itemDef = RoR2Content.Items.TeleportWhenOob,
+                        count = 1
+                    },
+                    new ItemCountPair()
+                    {
+                        itemDef = MagmaWormChanges.StatItem,
+                        count = 1
+                    },
+                };
+            }
+            else
+            {
+                spawnCard.itemsToGrant = new ItemCountPair[]
+                {
+                    new ItemCountPair()
+                    {
+                        itemDef = RoR2Content.Items.TeleportWhenOob,
+                        count = 1
+                    }
+                };
+            }
+
             spawnCard.equipmentToGrant = new EquipmentDef[]
             {
-                Addressables.LoadAssetAsync<EquipmentDef>("RoR2/DLC1/EliteEarth/EliteEarthEquipment.asset").WaitForCompletion()
+                EliteEarthEquipment
             };
 
             spawnCard.forbiddenAsBoss = true;
